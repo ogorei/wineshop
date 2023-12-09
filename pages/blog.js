5c20463b8acd946f9dd049a4a841632d3e2f8c6f
ogorei <reinasuzuki853@gmail.com>
2023-12-09 20:20:43 +0900

    updated blogpage

--- pages/blog.js
+++ pages/blog.js
@@ -1,28 +1,40 @@
-import React, { useState }  from 'react'
-import { client } from '../lib/client'
+import React, { useState } from "react";
+import { client, urlFor } from "../lib/client";
+import Link from "next/link";
 
-const Blog = ({ products }) => {
-  const [isFolded, setIsFolded] = useState(true)
-  const toggleFold = () => {
-    setIsFolded(!isFolded);
-  };
-
-  return (
-  
-    <div>blog</div>
-  )
-}
-
+const Blog = ({ posts }) => {
+	return (
+		<div>
+			{posts.map(post => {
+				return (
+					<Link href={`/post/${post.slug.current}`}>
+						<div className="flex flex-col md:flex-row m-6 border border-b-8 border-b-orange-800">
+							<div className="w-full md:w-2/5">
+								<img src={urlFor(post.image[0])} />
+							</div>
+							<div className="p-4">
+								<h1 className="font-bold">
+									{post.title}
+								</h1>
+								<p>
+									{post.overview}
+								</p>
+							</div>
+						</div>
+					</Link>
+				);
+			})}
+		</div>
+	);
+};
 
 export const getServerSideProps = async () => {
-  const query = '*[_type == "product"]'
-  const products = await client.fetch(query)
-  const bannerQuery = '*[_type == "banner"]'
-  const bannerData = await client.fetch(bannerQuery)
+	const query = '*[_type == "post"]';
+	const posts = await client.fetch(query);
 
-  return {
-    props: { products, bannerData }
-  }
-}
+	return {
+		props: { posts }
+	};
+};
 
 export default Blog;
